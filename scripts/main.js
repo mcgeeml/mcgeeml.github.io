95ca3a3ee7f6c0e4a64b42190c1b79db00af048d
mcgeeml <mcgeeml@rose-hulman.edu>
2021-08-15 23:32:33 -0400

    added side menu

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,12 @@ document.onreadystatechange = function() {
         document.querySelector("body").style.visibility = "visible";
     }
 };
-
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
